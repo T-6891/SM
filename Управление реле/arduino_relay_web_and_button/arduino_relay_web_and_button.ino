e95aa1fea921f8dc04ce2f1f7c2dbceb09986ffa
T-6891 <master@T-brain.ru>
2015-08-02 01:09:14 +0300

    fix

--- Управление реле/arduino_relay_web_and_button/arduino_relay_web_and_button.ino
+++ Управление реле/arduino_relay_web_and_button/arduino_relay_web_and_button.ino
@@ -2,25 +2,24 @@
 #include <Ethernet.h>
 #include <Bounce.h>
 
-#define BUTTON 2
-//создаем объект класса Bounce. Указываем пин, к которому подключена кнопка, и время дребезга в мс.
-Bounce bouncer = Bounce(BUTTON,5); 
+#define BUTTON 2                                       // создаем объект класса Bounce. Указываем пин, к которому подключена кнопка.
+Bounce bouncer = Bounce(BUTTON,5);                     // время дребезга контактов в мс
 
-int led = 4;
-int ledValue = LOW;
-byte mac[] = { 0xDE, 0xAD, 0xDE, 0xEF, 0xF2, 0xED };   //physical mac address
-byte ip[] = { 192, 168, 88, 170 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
-byte gateway[] = { 192, 168, 88, 1 };                   // internet access via router
-byte subnet[] = { 255, 255, 255, 0 };                  //subnet mask
-EthernetServer server(80);                             //server port     
+int relay = 4;                                         // пин подключения реле
+int relayValue = LOW;                                  // состояние реле при включении (выключено)
+byte mac[] = { 0xDE, 0xAD, 0xDE, 0xEF, 0xF2, 0xED };   // MAC адрес
+byte ip[] = { 192, 168, 88, 170 };                     // IP адрес
+byte gateway[] = { 192, 168, 88, 1 };                  // шлюз
+byte subnet[] = { 255, 255, 255, 0 };                  // маска сети
+EthernetServer server(80);                             // порт веб сервера     
 String readString;
 
 void setup() {
   Serial.begin(9600);
    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ;
   }
-  pinMode(led, OUTPUT);
+  pinMode(relay, OUTPUT);
   pinMode(BUTTON,INPUT);
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
@@ -31,47 +30,44 @@ void setup() {
 
 void loop() {
 
+// Упраление кнопками
   if ( bouncer.update() ) {
     if ( bouncer.read() == HIGH) {
-     if ( ledValue == LOW ) {
-       ledValue = HIGH;
+     if ( relayValue == LOW ) {
+       relayValue = HIGH;
      } else {
-       ledValue = LOW;
+       relayValue = LOW;
      }
-     digitalWrite(led,ledValue);
+     digitalWrite(relay,relayValue);
     }
   }
-  // Create a client connection
+
+// Ethernet сервер
   EthernetClient client = server.available();
           if (client) {
           while (client.connected()) {   
           if (client.available()) {
           char c = client.read();
      
-          //read char by char HTTP request
           if (readString.length() < 100) {
-            //store characters to string
             readString += c;
-            //Serial.print(c);
            }
 
-           //if HTTP request has ended
+// Вывод на com порт отладочной информации
            if (c == '\n') {          
-           Serial.println(readString); //print to serial monitor for debuging
-     
-           client.println("HTTP/1.1 200 OK"); //send new page
+           Serial.println(readString); 
+
+// Генерация веб страницы     
+           client.println("HTTP/1.1 200 OK");
            client.println("Content-Type: text/html");
            client.println();     
            client.println("<HTML>");
            client.println("<HEAD>");
            client.println("<meta charset='UTF-8' />");
-           client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
-           client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
-           client.println("<link rel='stylesheet' type='text/css' href='http://randomnerdtutorials.com/ethernetcss.css' />");
-           client.println("<TITLE>Web panel</TITLE>");
+           client.println("<TITLE>Панель управления</TITLE>");
            client.println("</HEAD>");
            client.println("<BODY>");
-           client.println("<H1>Web panel</H1>");
+           client.println("<H1>Панель управления</H1>");
            client.println("<hr />");
            client.println("<br />");  
            client.println("<H2>Arduino with Ethernet Shield</H2>");
@@ -84,11 +80,13 @@ void loop() {
      
            delay(1);
            client.stop();
+
+// Обработка http запросов
            if (readString.indexOf("?button1on") >0){
-               digitalWrite(led, HIGH);
+               digitalWrite(relay, HIGH);
            }
            if (readString.indexOf("?button1off") >0){
-               digitalWrite(led, LOW);
+               digitalWrite(relay, LOW);
            }
            
             readString="";  
