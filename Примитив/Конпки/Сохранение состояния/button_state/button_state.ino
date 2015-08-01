5cee7cef28d7491b37648b8f2e249299d547cd6c
T-6891 <master@T-brain.ru>
2015-08-01 18:46:29 +0300

    add example button bounce state

--- Примитив/Конпки/Сохранение состояния/button_state/button_state.ino
+++ Примитив/Конпки/Сохранение состояния/button_state/button_state.ino
@@ -1,35 +1,39 @@
-int flag=0; 
+//подключаем библиотеку Bounce
+#include <Bounce.h>
+//дефайним значения пинов с кнопкой и со светодиодом
+#define BUTTON 2
+#define LED 13
+//создаем объект класса Bounce. Указываем пин, к которому подключена кнопка, и время дребезга в мс.
+Bounce bouncer = Bounce(BUTTON,5); 
 
-void setup()    
-{ 
-       
-    pinMode(13, OUTPUT);         
-      
-        
-} 
+//задаем начальное состояние светодиода "выключен"
+int ledValue = LOW;
 
-void loop() 
 
-{ 
-        
-     if(digitalRead(14)==HIGH&&flag==0)//если кнопка нажата    
-     // и перемення flag равна 0 , то ... 
-     { 
-           
-       digitalWrite(13,!digitalRead(13)); 
-       flag=1; 
-        //это нужно для того что бы с каждым нажатием кнопки 
-        //происходило только одно действие 
-        // плюс защита от "дребезга"  100% 
-        
-     } 
-        
-      if(digitalRead(14)==LOW&&flag==1)//если кнопка НЕ нажата 
-     //и переменная flag равна - 1 ,то ... 
-     { 
-           
-        flag=0;//обнуляем переменную flag 
-     } 
-} 
 
+void setup() {
+  //определяем режимы работы пинов
+  pinMode(BUTTON,INPUT);
+  pinMode(LED,OUTPUT);
+}
+
+
+
+void loop() {
+  //если сменилось состояние кнопки
+  if ( bouncer.update() ) {
+    //если считано значение 1
+    if ( bouncer.read() == HIGH) {
+     //если свет был выключен, будем его включать
+     if ( ledValue == LOW ) {
+       ledValue = HIGH;
+     //если свет был включен, будем выключать
+     } else {
+       ledValue = LOW;
+     }
+     //записываем значение вкл/выкл на пин со светодиодом 
+     digitalWrite(LED,ledValue);
+    }
+  }
+}
 
