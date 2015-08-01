fdfe3c84b426ef1d1b584ed4053bfb71952a5681
T-6891 <master@T-brain.ru>
2015-08-01 23:09:15 +0300

    update

--- Управление реле/arduino_relay_web_and_button/arduino_relay_web_and_button.ino
+++ Управление реле/arduino_relay_web_and_button/arduino_relay_web_and_button.ino
@@ -1,10 +1,3 @@
-/*
- Created by Rui Santos
- Visit: http://randomnerdtutorials.com for more arduino projects
-
- Arduino with Ethernet Shield
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
 #include <Bounce.h>
@@ -15,7 +8,7 @@ Bounce bouncer = Bounce(BUTTON,5);
 
 int led = 4;
 int ledValue = LOW;
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };   //physical mac address
+byte mac[] = { 0xDE, 0xAD, 0xDE, 0xEF, 0xF2, 0xED };   //physical mac address
 byte ip[] = { 192, 168, 88, 170 };                      // ip in lan (that's what you need to use in your browser. ("192.168.1.178")
 byte gateway[] = { 192, 168, 88, 1 };                   // internet access via router
 byte subnet[] = { 255, 255, 255, 0 };                  //subnet mask
@@ -23,14 +16,12 @@ EthernetServer server(80);                             //server port
 String readString;
 
 void setup() {
- // Open serial communications and wait for port to open:
   Serial.begin(9600);
    while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   pinMode(led, OUTPUT);
   pinMode(BUTTON,INPUT);
-  // start the Ethernet connection and the server:
   Ethernet.begin(mac, ip, gateway, subnet);
   server.begin();
   Serial.print("server is at ");
@@ -40,37 +31,32 @@ void setup() {
 
 void loop() {
 
-  //если сменилось состояние кнопки
   if ( bouncer.update() ) {
-    //если считано значение 1
     if ( bouncer.read() == HIGH) {
-     //если свет был выключен, будем его включать
      if ( ledValue == LOW ) {
        ledValue = HIGH;
-     //если свет был включен, будем выключать
      } else {
        ledValue = LOW;
      }
-     //записываем значение вкл/выкл на пин со светодиодом 
      digitalWrite(led,ledValue);
     }
   }
   // Create a client connection
   EthernetClient client = server.available();
-  if (client) {
-    while (client.connected()) {   
-      if (client.available()) {
-        char c = client.read();
+          if (client) {
+          while (client.connected()) {   
+          if (client.available()) {
+          char c = client.read();
      
-        //read char by char HTTP request
-        if (readString.length() < 100) {
-          //store characters to string
-          readString += c;
-          //Serial.print(c);
-         }
+          //read char by char HTTP request
+          if (readString.length() < 100) {
+            //store characters to string
+            readString += c;
+            //Serial.print(c);
+           }
 
-         //if HTTP request has ended
-         if (c == '\n') {          
+           //if HTTP request has ended
+           if (c == '\n') {          
            Serial.println(readString); //print to serial monitor for debuging
      
            client.println("HTTP/1.1 200 OK"); //send new page
@@ -78,27 +64,26 @@ void loop() {
            client.println();     
            client.println("<HTML>");
            client.println("<HEAD>");
+           client.println("<meta charset='UTF-8' />");
            client.println("<meta name='apple-mobile-web-app-capable' content='yes' />");
            client.println("<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />");
            client.println("<link rel='stylesheet' type='text/css' href='http://randomnerdtutorials.com/ethernetcss.css' />");
-           client.println("<TITLE>Random Nerd Tutorials Project</TITLE>");
+           client.println("<TITLE>Web panel</TITLE>");
            client.println("</HEAD>");
            client.println("<BODY>");
-           client.println("<H1>Random Nerd Tutorials Project</H1>");
+           client.println("<H1>Web panel</H1>");
            client.println("<hr />");
            client.println("<br />");  
            client.println("<H2>Arduino with Ethernet Shield</H2>");
            client.println("<br />");  
-           client.println("<a href=\"/?button1on\"\">Turn On LED</a>");
-           client.println("<a href=\"/?button1off\"\">Turn Off LED</a><br />");   
+           client.println("<a href=\"/?button1on\"\">Включить</a>");
+           client.println("<a href=\"/?button1off\"\">Выключить</a><br />");   
            client.println("<br />");     
            client.println("</BODY>");
            client.println("</HTML>");
      
            delay(1);
-           //stopping client
            client.stop();
-           //controls the Arduino if you press the buttons
            if (readString.indexOf("?button1on") >0){
                digitalWrite(led, HIGH);
            }
@@ -106,8 +91,6 @@ void loop() {
                digitalWrite(led, LOW);
            }
            
-
-            //clearing string for next read
             readString="";  
            
          }
